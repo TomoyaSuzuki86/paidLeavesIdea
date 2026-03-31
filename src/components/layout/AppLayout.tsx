ffe2cdefee737df44caa38e9c87b0f046696e6f9
CATRABBITHP\tomo1 <tmgo31586w@gmail.com>
2026-03-31 20:51:21 +0900

    Reset scroll on route change

--- src/components/layout/AppLayout.tsx
+++ src/components/layout/AppLayout.tsx
@@ -1,6 +1,17 @@
+import { useEffect } from "react";
 import { Outlet, useLocation } from "react-router-dom";
 import { Footer } from "./Footer";
 import { Header } from "./Header";
+
+function ScrollToTopOnRouteChange() {
+  const location = useLocation();
+
+  useEffect(() => {
+    window.scrollTo({ top: 0, left: 0, behavior: "auto" });
+  }, [location.pathname]);
+
+  return null;
+}
 
 export function AppLayout() {
   const location = useLocation();
@@ -8,6 +19,7 @@ export function AppLayout() {
 
   return (
     <>
+      <ScrollToTopOnRouteChange />
       <Header />
       <Outlet />
       {!isDiagnosticRoute ? <Footer /> : null}
